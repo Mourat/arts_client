3a7bd24cd71429e0047a2fb823a5484d3c5c52d9
Mourat <mourat@boutry.me>
2023-08-25 15:58:50 +0200

    Added actual art's information for updating page

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import axios from "axios"
 import {Link, useLocation, useNavigate} from "react-router-dom"
 
@@ -16,6 +16,19 @@ const Update = () => {
     const navigate = useNavigate()
     const location = useLocation()
     const artId = location.pathname.split("/")[2]
+
+    useEffect(() => {
+        const fetchArtById = async () => {
+            try {
+                const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/arts/${artId}`)
+                setArt(res.data[0])
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchArtById()
+    }, [artId]);
+
 
     const handleChange = (e) => {
         setArt((prev) => ({...prev, [e.target.name]: e.target.value}))
@@ -35,10 +48,10 @@ const Update = () => {
     return (
         <div className="form">
             <h1>Update Art</h1>
-            <input type="text" name="title" onChange={handleChange} placeholder="Title"/>
-            <textarea name="desc" rows={5} onChange={handleChange} placeholder="Description"/>
-            <input type="number" name="price" onChange={handleChange} placeholder="Price"/>
-            <input type="text" name="cover" placeholder="Cover picture URL with http" onChange={handleChange}/>
+            <input type="text" name="title" onChange={handleChange} placeholder="Title" value={art.title}/>
+            <textarea name="desc" rows={5} onChange={handleChange} placeholder="Description" value={art.desc}/>
+            <input type="number" name="price" onChange={handleChange} placeholder="Price" value={art.price}/>
+            <input type="text" name="cover" placeholder="Cover picture URL with http" onChange={handleChange} value={art.cover}/>
             <button className="formButton" onClick={handleClick}>Update</button>
             {error && "Something went wrong!"}
             <Link to="/">See gallery</Link>
